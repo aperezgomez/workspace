e24e518de62896653d8869e9085b1d5ea3e484b0
aperezgomez <alexispere@gmail.com>
2023-08-20 21:11:27 -0400

    apis

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField(default=0)
+    BookingDate = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+class Menu(models.Model):
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    Inventory = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Menu
+from .serializers import MenuSerializer
+from .models import Booking
+from .serializers import BookingSerializer
+from rest_framework import viewsets
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
+
+# Declare the MenuItemsView class to handle POST and GET methods.
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+# Declare the SingleMenuItemView class to process GET, PUT, and DELETE methods.
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
